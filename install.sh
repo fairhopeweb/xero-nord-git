9b6746eb3556664df5de19602687821dabd01347
XeroLinux <steve@techxero.com>
2022-06-21 16:20:53 +0300

    Latte

--- install.sh
+++ install.sh
@@ -10,11 +10,11 @@ cp -Rf ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S)
 sleep 2
 echo "Installing required Tools"
 echo "#################################"
-sudo pacman -S --needed --noconfirm kvantum latte-dock neofetch yakuake
+sudo pacman -S --needed --noconfirm kvantum latte-dock-git neofetch yakuake
 sleep 2
 echo "Installing Nord Theme"
 echo "#################################"
-sudo pacman -S --needed --noconfirm kvantum-theme-nordic-git nordic-darker-standard-buttons-theme nordic-darker-theme nordic-kde-git nordic-theme sddm-nordic-theme-git zafiro-icon-theme nordic-wallpapers
+sudo pacman -S --needed --noconfirm kvantum-theme-nordic-git nordic-darker-standard-buttons-theme nordic-darker-theme-git nordic-kde-git nordic-theme sddm-nordic-theme-git nordic-kde-transparency-git zafiro-icon-theme nordic-wallpapers
 sleep 2
 echo "Installing Fonts"
 echo "#################################"
