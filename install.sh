952a59f459ff9ee89acb81f6c13f6a898e15a042
TechXero *~* DarkXero <81101439+TechXero@users.noreply.github.com>
2022-07-17 19:55:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "##########################################"
 sleep 5
 echo "Creating Backups of ~/.config folder"
 echo "#####################################"
-cp -Rf ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S)
+cp -r ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S)
 sleep 2
 echo "Removing old Latte-Dock & Lightly"
 echo "#################################"
